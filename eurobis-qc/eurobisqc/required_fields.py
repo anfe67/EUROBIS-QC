f348f32f84621791fdc8a41039890f52acf543e6
anto <antonio.ferraro@safits.be>
2020-12-02 18:27:02 +0100

    Added verification of good BasisOfRecord for the source records.

--- eurobis-qc/eurobisqc/required_fields.py
+++ eurobis-qc/eurobisqc/required_fields.py
@@ -2,6 +2,8 @@ from . util import qc_flags
 import logging
 logger = logging.getLogger(__name__)
 
+# Return value in case of errors
+error_mask = qc_flags.QCFlag.REQUIRED_FIELDS.encode()
 
 def check_record(record):
     """Check for presence of required fields."""
@@ -16,7 +18,23 @@ def check_record(record):
     if len(present_required_fields) == len(required_fields):
         result = True
 
-    return 0 if result else qc_flags.QCFlag.REQUIRED_FIELDS.encode()
+    return 0 if result else error_mask
+
+def check_source_record(record):
+
+    """ To be called for source type records """
+
+    vocab = ["PreservedSpecimen", "FossilSpecimen", "LivingSpecimen", "MaterialSample", "Event", "HumanObservation", "MachineObservation", "Taxon", "Occurrence"]
+    if "basisOfRecord" in record and record["basisOfRecord"] is not None:
+        if not record["basisOfRecord"].lower() in [value.lower() for value in vocab]:
+            return error_mask
+    else:
+        return error_mask
+
+    return 0
 
 def check(records):
     return [check_record(record) for record in records]
+
+def check_source_records(records):
+    return [check_source_record(source_record) for source_record in records]
--- eurobis-qc/eurobisqc/test/test_required_fields.py
+++ eurobis-qc/eurobisqc/test/test_required_fields.py
@@ -7,10 +7,20 @@ class Test(TestCase):
         {"id": 0}, # No fields
         {"id": 1, "eventDate": "1969-06-11", "decimalLongitude": 32.4, "decimalLatitude": 1.0, "scientificName":
             "Zooplankton", "scientificNameID": "urn:lsid:marinespecies.org:taxname:1",
-            "occurrenceStatus": "present","basisOfRecord": "MaterialSample"}, # All fields
+         "occurrenceStatus": "present","basisOfRecord": "MaterialSample"}, # All fields
         {"id": 2, "eventDate": "1969-06-11", "decimalLongitude": 32.4, "decimalLatitude": 1.0, "scientificName":
             "Zooplankton", "occurrenceStatus": "present","basisOfRecord": "MaterialSample"}, # Some fields
-        {"id": 3, "basisOfRecord": "human observation"} # Another with not so many fields
+        {"id": 3, "basisOfRecord": "human observation"}, # Another with not so many fields
+        {"id": "Het Zwin (Knokke)_9_9_5", "basisOfRecord": "MaterialSample",
+         "occurrenceID": "Het Zwin (Knokke)_9_9_5_6_absent", "occurrenceStatus": "absent",
+         "eventID": "Het Zwin (Knokke)_9_9_5", "scientificName": "Tropidia cita"}, # Source record OK
+        {"id": "Het Zwin (Knokke)_9_9_5", "basisOfRecord": "Materample",
+         "occurrenceID": "Het Zwin (Knokke)_9_9_5_5_absent", "occurrenceStatus": "absent",
+         "eventID": "Het Zwin (Knokke)_9_9_5", "scientificName": "Syrphu balteatu"
+         }, # Source record wrong basisOfRecord
+        {"id": "Het Zwin (Knokke)_9_9_5", "occurrenceID": "Het Zwin (Knokke)_9_9_5_5_absent",
+         "occurrenceStatus": "absent", "eventID": "Het Zwin (Knokke)_9_9_5", "scientificName": "Syrphu balteatu"
+         },  # Source record no basisOfRecord
     ]
     def test_check_record(self):
         self.assertTrue(required_fields.check_record(self.records[0]) == 1)
@@ -18,9 +28,20 @@ class Test(TestCase):
         self.assertTrue(required_fields.check_record(self.records[2]) == 1)
         self.assertTrue(required_fields.check_record(self.records[3]) == 1)
 
+    def test_source_record(self):
+        # Source Records
+        self.assertTrue(required_fields.check_source_record(self.records[4]) == 0)
+        self.assertTrue(required_fields.check_source_record(self.records[5]) == 1)
+        self.assertTrue(required_fields.check_source_record(self.records[6]) == 1)
+
+    def test_check_source_records(self):
+        self.assertTrue(required_fields.check_source_records(self.records[4:]) == [0, 1, 1])
 
     def test_check(self):
         # Comparison is OK as we want to see element by element
-        self.assertTrue(required_fields.check(self.records) == [1,0,1,1])
+        self.assertTrue(required_fields.check(self.records[0:4]) == [1,0,1,1])
+
+
+
 
 
